d2ad14a2a04dbcdcfecba1348e4e5c0d3b0c2f5f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-10 16:26:23 +0100

    Test if user object is instantiated

--- User/user.test.js
+++ User/user.test.js
@@ -1,1 +1,8 @@
 var User = require("./user");
+
+describe("User Object Tests", function() {
+  var felix = new User();
+  test("should be a construction function", function() {
+    expect(felix instanceof User).toBe(true);
+  });
+});
