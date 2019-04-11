a99401d97332d90e8e957b674b6b69e0b27165b1
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-11 09:04:05 +0100

    Write test to confirm if admin instanceof user is true

---
+++ test/admin.test.js
@@ -0,0 +1,22 @@
+var User = require("../User/user");
+var Admin = require("../User/admin");
+
+describe("Admin Object Tests", function() {
+  var kingsley = new Admin("Kingsley", "kingsley@gmail.com", "king");
+  var mike = new Admin("Mike", "mike@gmail.com", "ross");
+
+  test("should inherit from User", function() {
+    expect(kingsley instanceof User).toBe(true);
+  });
+
+  test("should return user object", function() {
+    var user1 = mike.getUser(mike.id);
+    expect(user1.email).toBe("mike@gmail.com");
+    expect(user1.password).toBe("ross");
+  });
+
+  test("should update user info", function() {
+    kingsley.updateUser("password", "renee");
+    expect(kingsley.password).toBe("renee");
+  });
+});
