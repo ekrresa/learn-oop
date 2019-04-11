28c9ba20ad36f63ccb01cf841f82c1d8b220062d
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-11 09:00:38 +0100

    renamed file

--- User/Admin.js
+++ User/Admin.js
@@ -1,10 +1,13 @@
 var db = require("../database");
-var User = require("./User");
+var User = require("./user");
 
 function Admin(name, email, password) {
   User.call(this, name, email, password);
-  db.usersDB[this.id].isAdmin = true;
+  this.isAdmin = true;
+  db.usersDB[this.id].isAdmin = this.isAdmin;
 }
 
 Admin.prototype = Object.create(User.prototype);
 Admin.prototype.constructor = Admin;
+
+module.exports = Admin;
